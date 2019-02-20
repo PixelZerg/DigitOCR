8069bc4ad808ef2417dd9c098ff3dd63d93dfbf8
PixelZerg <pixelzerg@gmail.com>
2019-02-20 16:02:41 +0000

    UI work

--- digitocr.py
+++ digitocr.py
@@ -1,98 +1,68 @@
 import sys
 
 from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QResizeEvent
 from PyQt5.QtWidgets import *
 
 class TestDialog(QMainWindow):
     def __init__(self):
         super().__init__(flags=Qt.WindowStaysOnTopHint)
         self.__init_ui()
+        self.setWindowModality(Qt.ApplicationModal)
+
+    def showEvent(self, event):
+        # on startup, resize
+        self.resizeEvent(None)
+
+    def resizeEvent(self, event:QResizeEvent):
+        print("resize")
+
+        area_size = self.area.size()
+        dh = area_size.width()-area_size.height()
+
+        cur_size = self.size()
+        self.resize(cur_size.width(),cur_size.height()+dh)
+
+        if event is not None:
+            QMainWindow.resizeEvent(self, event)
 
     def __init_ui(self):
-        # core = QWidget()
-        # layout = QVBoxLayout()
-        #
-        # layout.addWidget(self.__init_info_bar())
-        # layout.addWidget(self.__init_button_bar())
-        #
-        # core.setLayout(layout)
-        # self.setCentralWidget(core)
-
-        # self.setCentralWidget(QTextEdit())
-        # self.addDockWidget(Qt.TopDockWidgetArea,self.__make_bar(
-        #     QLabel("wow")
-        # ))
         core = QWidget()
         layout = QVBoxLayout()
 
         bar = QWidget()
         bar.layout = QHBoxLayout()
-        # but = QPushButton("wow")
-        # but.setContentsMargins(0,0,0,0)
-        bar.layout.addWidget(QPushButton("wow"))
-        bar.layout.addWidget(QPushButton("ROO"))
-        bar.layout.setContentsMargins(0,0,0,0)
-        bar.setLayout(bar.layout)
-        # bar.setFixedHeight(bar.layout.sizeHint().height())
-        # bar.layout.setContentsMargins(0,0,0,0)
-        # bar.setContentsMargins(0,0,0,0)
-        # bar.layout.setSpacing(0) # between buttons
 
-        layout.addWidget(bar)
-        layout.addWidget(QTextEdit())
+        self.label = QLabel("Draw a digit:")
+        layout.addWidget(self.__make_bar(self.label))
+
+        self.area = QTextEdit()
+        layout.addWidget(self.area)
+
+        self.btn_clear = QPushButton("Clear")
+        self.btn_run = QPushButton("Run")
+
+        layout.addWidget(self.__make_bar(self.btn_clear, self.btn_run))
 
         core.setLayout(layout)
         self.setCentralWidget(core)
 
     def __make_bar(self, *widgets):
         bar = QWidget()
-        layout = QHBoxLayout()
-        # layout.setContentsMargins(5,10,5,5)
+        bar.layout = QHBoxLayout()
 
         for widget in widgets:
-            widget.setAlignment(Qt.AlignVCenter)
-            layout.addWidget(widget)
+            bar.layout.addWidget(widget)
 
-        bar.setLayout(layout)
-
-        ret = QDockWidget()
-        ret.setTitleBarWidget(QWidget()) # remove QDockWidget title
-        ret.setWidget(bar)
-        ret.setContentsMargins(0,0,0,0)
-
-        return ret
-
-    def __init_info_bar(self):
-        top_bar = QWidget()
-        layout = QHBoxLayout()
-        layout.setContentsMargins(0,0,0,0)
-        layout.setSpacing(0)
-
-        layout.addWidget(QLabel("asdasdsadadsasd"))
-        layout.addWidget(QLabel("asdasd"))
-
-        top_bar.setLayout(layout)
-
-        ret = QDockWidget()
-        ret.setTitleBarWidget(QWidget()) # remove QDockWidget title
-        ret.setWidget(top_bar)
-        ret.setContentsMargins(0,0,0,0)
-        return ret
-
-    def __init_button_bar(self):
-        top_bar = QDockWidget()
-        layout = QHBoxLayout()
-
-        layout.addWidget(QPushButton("wow"))
-        layout.addWidget(QPushButton("asdasd"))
-
-        top_bar.setLayout(layout)
-        return top_bar
+        bar.layout.setContentsMargins(2, 0, 2, 3)
+        bar.setLayout(bar.layout)
+        return bar
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
 
     ui = TestDialog()
+    ui.resize(320, 320)
     ui.show()
 
     sys.exit(app.exec())
